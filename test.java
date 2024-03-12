970fba8e0ec447ce4a2b7dd1f6304d1cd85e839f
TriTin <tinnguyen5071@gmail.com>
2024-03-12 17:07:45 +0700

    Chỉnh sửa lại AES

--- test.java
+++ test.java
@@ -1,15 +1,21 @@
+import java.util.Scanner;
+
 /**
  * test
  */
 public class test {
         public static void main(String[] args) {
-                String secretKey = "TVD";
-                String originalString = "teamvietdev.com";
+                Scanner sc = new Scanner(System.in);
 
+                System.out.println("Hãy nhập khóa");
+                String secretKey = sc.nextLine();
+                System.out.println("Hãy nhập chuỗi");
+                String originalString = sc.nextLine();
                 TestAES testAES = new TestAES();
                 String encryptedString = testAES.encrypt(originalString, secretKey);
-                System.out.println("Encrypt: " + encryptedString);
+                System.out.println("Mã hóa: " + encryptedString);
                 String decryptedString = testAES.decrypt(encryptedString, secretKey);
-                System.out.println("Decrypt: " + decryptedString);
+                System.out.println("Giải mã: " + decryptedString);
+                sc.close();
         }
 }
